22c7521366e60ffb3b8f06ce0a9dabbf9477d539
Quang Huy <tranquanghuyblackberry@gmail.com>
2023-09-09 16:49:24 +0700

    complete

--- javascript/exercise.js
+++ javascript/exercise.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+  if(parseInt(get_params()['id']) > 174 || parseInt(get_params()['id']) < 1 ){
+    window.location.href = "/";
+  }
+
   $.getJSON('./database/exercises/ex_' + get_params()['id'] + '.json', function(teams) {
     var ansArray = [];
 
@@ -33,6 +37,41 @@ $(document).ready(function() {
       $('#exercise-' + i).append("<div class='box-button mgt-20 d-flex justify-content-end'><button class='btn btn-danger mgr-20' onclick='reset_exersice(" + i +")'> Restart </button><button class='btn btn-success' onclick='submit_exersice(" + i +")'> Submit </button></div>")
 
     });
+  });
+
+
+  $.getJSON('./database/general_topics.json', function(groups){
+    if(parseInt(get_params()['id']) > 111){
+      return
+    }else{
+      var id = parseInt(get_params()['id']);
+      var k = 0;
+      groups.map(function(group, index){
+        if(id >= parseInt(group['id_start'])){
+          k = index;
+          return;
+        }
+      });
+
+      $('#back_word_list').attr("href", "topic.html?topic=" + groups[k]['brief'])
+    }
+  });
+
+  $.getJSON('./database/academic_topics.json', function(groups){
+    if(parseInt(get_params()['id']) < 112){
+      return
+    }else{
+      var id = parseInt(get_params()['id']);
+      var k = 0;
+      groups.map(function(group, index){
+        if(id >= parseInt(group['id_start'])){
+          k = index;
+          return;
+        }
+      });
+
+      $('#back_word_list').attr("href", "topic.html?topic=" + groups[k]['brief'])
+    }
   });
 
 });
--- javascript/topic.js
+++ javascript/topic.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
   $.getJSON('./database/groups/' + get_params()['topic'] + '.json', function(object) {
     $('.title').text(object.title)
-    console.log(object)
     object.exercises.map(function (exercise, index) {
 
       let html = "";
@@ -11,8 +10,17 @@ $(document).ready(function() {
       });
 
       $('#topics').append("<div class='col-sm-12 col-md-6'><a href='./exercise.html?id=" + exercise.id + "' ><div class='exercise'>" + html + "</div></a></div>" )
-    });
-  });
+    })
+  })
+  .fail(function() { window.location.href = "/"; });
+
+  const general_topics = ['all_adjective', 'arts_entertainment_literature', 'basic_science_academia', 'crime_the_law', 'feeling_qualities_states', 'food_eating', 'heal_the_body', 'lots_of_nouns', 'money_work', 'nouns_verbs_both', 'phrasal_verbs', 'structures_places_objects', 'the_natural_wolrd', 'the_political_world', 'the_social_world', 'transitions', 'verbs_verbs_and_verbs', 'war_violence_conflict']
+
+  if(general_topics.includes(get_params()['topic'])){
+    $('#back_to_category').attr("href", "./general_vocabulary.html")
+  }else{
+    $('#back_to_category').attr("href", "./academic_vocabulary.html")
+  }
 });
 
 
