32e06070f064737a8fa9927d03f5708e8e83e466
Quang Huy <tranquanghuyblackberry@gmail.com>
2024-08-03 21:23:02 +0700

    Rebuild

--- javascript/academic.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $.getJSON('./database/academic_topics.json', function(topics) {
-    topics.map(function (topic) {
-      $('#academic').append("<div class='col-xs-12 col-sm-6 sol-md-4 col-lg-2'><a href='./topic.html?topic=" + topic.brief + "' ><div class='topic'>" + topic.name + "</div></a></div>")
-    });
-  });
-});
--- javascript/category.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  $.getJSON('./database/general_topics.json', function(topics) {
-    topics.map(function (topic) {
-      $('#groups_general').append("<div class='col-xs-12 col-sm-6 sol-md-4 col-lg-2'><a href='./topic.html?topic=" + topic.brief + "' ><div class='topic'>" + topic.name + "</div></a></div>");
-    });
-  });
-});
